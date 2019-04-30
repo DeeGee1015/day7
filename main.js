af0a3ca55d29f8d63099d9b99ba5eb6bc330e7fd
Devin Guerrero <devinguerrero1015@gmail.com>
2019-04-30 14:51:59 -0400

    First commit

---
+++ main.js
@@ -0,0 +1,22 @@
+function LightTheme() {
+  document.body.style.backgroundColor = '#fff'
+  document.body.style.Color = '#333'
+}
+
+function darkTheme() {
+  document.body.style.backgroundColor = '#333'
+  document.body.style.color = 'white'
+}
+
+function sepiaTheme() {
+  document.body.style.backgroundColor = 'cc9d68'
+  document.body.style.Color = '#333'
+}
+
+function turnpage() {
+  let screen = document.getElementById('screen')
+  screen.classList.add('page')
+setTimeout(function() {
+  screen.classList.remove('page')
+}, 1400)
+}
